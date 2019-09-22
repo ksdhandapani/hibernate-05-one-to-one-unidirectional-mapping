c3b06031dd27e1fe75caad5f221e6ee6ff824790
Dhandapani Sudhakar <52704352+ksdhandapani@users.noreply.github.com>
2019-09-22 19:51:42 +0530

    Initial Commit

---
+++ src/main/java/com/sakdd/Hibernate05OneToOneMappingApplication.java
@@ -0,0 +1,13 @@
+package com.sakdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate05OneToOneMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate05OneToOneMappingApplication.class, args);
+	}
+
+}
